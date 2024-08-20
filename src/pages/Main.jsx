d39a57f1fed0c3e95a4cd33b53f64a1188f178df
Ajaym1618 <ajaysiri54@gmail.com>
2024-08-20 19:16:15 +0530

    first commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -21,11 +21,11 @@ const Main = () => {
   return (
     <div className="w-[100%] h-[88vh] bg-white flex max-sm:flex-col max-sm:justify-center max-sm:h-[88vh]">
       <div className="w-[100%] h-full pl-32 flex flex-col gap-8 justify-center items-start text-[#0275a4] max-lg:pl-10 max-sm:py-0 max-sm:gap-4 max-sm:pl-6 max-sm:h-auto">
-        <h1 className="text-5xl font-light max-lg:text-4xl max-sm:text-3xl">Hi,</h1>
-        <h1 className="text-8xl max-lg:text-6xl max-sm:text-4xl">
+        <h1 className="text-5xl font-light max-lg:text-4xl">Hi,</h1>
+        <h1 className="text-8xl max-lg:text-6xl max-sm:text-5xl">
           I'm <span className="text-[#c12f2f]">Ajay</span>
         </h1>
-        <h1 className="text-5xl font-light text-[#c12f2f] max-lg:text-2xl max-sm:text-lg">
+        <h1 className="text-5xl font-light text-[#c12f2f] max-lg:text-2xl max-sm:text-xl">
           I am a <span ref={el} className="text-[#0275a4]"></span>
         </h1>
         <p className="text-2xl font-light max-lg:text-xl max-sm:text-[16px]">
@@ -33,7 +33,7 @@ const Main = () => {
         </p>
       </div>
       <div className="w-[100%] h-full flex justify-center items-center max-sm:items-start max-sm:pt-6 max-sm:h-auto">
-        <div className="w-[70%] max-sm:w-[80%]">
+        <div className="w-[70%] max-sm:w-[90%]">
           <img src={home} alt="coding" className="w-[100%]" />
         </div>
       </div>
