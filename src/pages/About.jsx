c9272da956be20449c6c02977ebd56f9dd204108
Ajaym1618 <ajaysiri54@gmail.com>
2024-08-22 15:59:39 +0530

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import cherry from "../assets/cherry.png";
+import resume from "../assets/Ajay.M.pdf";
 
 const About = () => {
   return (
     <div className="w-[100%] h-[88vh] flex flex-col items-center py-8 max-xl:py-4 max-sm:h-auto max-sm:py-8">
       <h1 className="text-5xl text-[#c12f2f] pb-4 max-lg:text-3xl">About Me</h1>
       <div className="w-full h-full flex max-sm:flex-col-reverse">
-        <div className="w-full h-full flex justify-center items-center max-lg:w-[70%] max-sm:w-full">
+        <div className="w-full h-full flex flex-col justify-start items-center max-lg:w-[70%] max-lg:pt-6 max-sm:w-full max-sm:flex-row max-sm:justify-between max-sm:px-4">
           <div className="w-[300px] max-lg:w-[200px] max-sm:pt-4 max-sm:w-[150px]">
-            <img src={cherry} alt="about" className="w-[100%]"/>
+            <img src={cherry} alt="about" className="w-[100%]" />
+          </div>
+          <div className="flex justify-center gap-5 mt-8 max-sm:flex-col">
+            <a
+              href={resume}
+              target="_blank"
+              className="flex justify-center py-2 px-4 bg-[#c12f2f] text-white border-2 border-[#c12f2f] rounded-md transition-all ease-linear duration-150 hover:text-[#0275a4] hover:bg-white hover:border-[#0275a4] max-sm:px-3 max-sm:py-2 max-sm:text-sm"
+            >
+              View Cv
+            </a>
+            <a
+              href={resume}
+              download={"Ajay.M-resume.pdf"}
+              className="py-2 px-4 bg-[#c12f2f] text-white border-2 border-[#c12f2f] rounded-md transition-all ease-linear duration-150 hover:text-[#0275a4] hover:bg-white hover:border-[#0275a4] max-sm:px-3 max-sm:py-2 max-sm:text-sm"
+            >
+              Download Cv
+            </a>
           </div>
         </div>
         <div className="w-full h-full flex items-center">
